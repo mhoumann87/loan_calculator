79c52bac26427cecb8311f843e82e512fa8ef902
Michael Houmann <mhoumann87@gmail.com>
2021-07-23 10:10:01 +0200

    Add function to check validiti and enable button :sparkles:

--- app.js
+++ app.js
@@ -7,13 +7,30 @@ const interest = document.querySelector('#interest');
 const feq = document.querySelector('#feq');
 
 // Set a variable for checking if all data is valid
-let isValid = true;
+let buttonDisabled = true;
+
+const allValid = {
+  amount: false,
+  startup: false,
+  duration: false,
+  interest: false,
+};
 
 /*
  * Function to validate our input. We only check for if there are an input
  * and that input is a number.
  */
 const validateInput = input => input.length > 0 && !isNaN(parseInt(input));
+
+/*
+ * Function to check all inputs are valid, and enable the button
+ */
+const checkValidity = () => {
+  const allItemsValid = Object.values(allValid).every(item => item === true);
+  //console.log(allItemsValid);
+  allItemsValid ? (buttonDisabled = false) : (buttonDisabled = true);
+  disableButton();
+};
 
 /*
  * Function to add red border in value not is valid
@@ -23,9 +40,23 @@ const markError = event => {
   const isValid = validateInput(event.target.value);
 
   if (event.target.id === 'years' || event.target.id === 'months') {
+    if (validateInput(years.value) || validateInput(months.value)) {
+      years.classList.remove('error');
+      months.classList.remove('error');
+      allValid.duration = true;
+    } else {
+      years.classList.add('error');
+      months.classList.add('error');
+    }
   } else {
-    isValid ? elm.classList.remove('error') : elm.classList.add('error');
+    if (isValid) {
+      elm.classList.remove('error');
+      allValid[event.target.id] = true;
+    } else {
+      elm.classList.add('error');
+    }
   }
+  checkValidity();
 };
 
 const formSubmit = e => {
@@ -36,7 +67,7 @@ const formSubmit = e => {
  * Disable the button if isValid is false
  */
 const disableButton = () =>
-  (document.querySelector('button').disabled = isValid);
+  (document.querySelector('button').disabled = buttonDisabled);
 
 /*
  * Get all input fields where the type is text, and add an event listener to them,
@@ -45,6 +76,11 @@ const disableButton = () =>
 document.querySelectorAll('input[type=text]').forEach(input => {
   input.addEventListener('blur', markError);
 });
+
+/*
+ * if user tabs away from the select input, run the check for the button
+ */
+// feq.addEventListener('blur', disableButton);
 
 /*
  * Start everything when the form is submitted
