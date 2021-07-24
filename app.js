e484aa90aba2b7460402c33cfd780cd79156d110
Michael Houmann <mhoumann87@gmail.com>
2021-07-24 11:43:58 +0200

    Add function to gather all input :sparkles:

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // Get "access" to the input fields on the page
 const amount = document.querySelector('#amount');
 const startup = document.querySelector('#startup');
+const running = document.querySelector('#running');
+const runningFeq = document.querySelector('#runningFeq');
 const years = document.querySelector('#years');
 const months = document.querySelector('#months');
 const interest = document.querySelector('#interest');
-const feq = document.querySelector('#feq');
+const interestFeq = document.querySelector('#interestFeq');
 
 // Set a variable for checking if all data is valid
 let buttonDisabled = true;
@@ -15,6 +17,11 @@ const allValid = {
   duration: false,
   interest: false,
 };
+
+/*
+ * Because running fee isn't always on loans, we set a zero from the beginning
+ */
+running.value = 0;
 
 /*
  * Function to validate our input. We only check for if there are an input
@@ -61,6 +68,24 @@ const markError = event => {
 
 const formSubmit = e => {
   e.preventDefault();
+  if (years.value === '') {
+    years.value = 0;
+  }
+  if (months.value === '') {
+    months.value = 0;
+  }
+  const values = {
+    amount: amount.value,
+    startup: startup.value,
+    running: running.value,
+    runningFeq: runningFeq.value,
+    years: years.value,
+    months: months.value,
+    interest: interest.value,
+    interestFeq: interestFeq.value,
+  };
+
+  console.log(values);
 };
 
 /*
